160cf72a7140275aa2e70071e55c5d5306a30e18
Arnav Rathore <arnav005singhrathore@gmail.com>
2026-07-06 01:57:36 +0530

    Add description

--- P2_formulae.py
+++ P2_formulae.py
@@ -1,17 +1,37 @@
-''' AREA OF PARALLELOGRAM '''
-# a = input("Base : ")
-# b = input("Height : ")
-# print("AREA OF PARALLELOGRAM =",int(a)*int(b),"square units")
+"""
+📐 Shape Area Calculator
 
-''' AREA OF RIGHT ANGLE TRIANGLE '''
-# c = input("Base : ")
-# d = input("Height : ")
-# print("AREA OF Δ =",0.5*int(c)*int(d), "square units")
+A Python mini-project that calculates the area of common geometric
+shapes using user-provided dimensions.
 
-''' AREA OF CIRCLE '''
-# e = input("Radius : ")
-# print("AREA OF CIRCLE =",22/7*int(e)*int(e), "square units" )
+Supported Shapes:
+• Parallelogram
+• Right Triangle
+• Circle
+• Square
 
-''' AREA OF SQUARE  '''
-# d = input("edge : ")
-# print("AREA OF SQUARE =",int(d)*int(d), "square units")
+Concepts Practiced:
+• input()
+• int()
+• Mathematical formulas
+• Arithmetic operators
+• print()
+"""
+
+# AREA OF PARALLELOGRAM
+a = input("Base : ")
+b = input("Height : ")
+print("AREA OF PARALLELOGRAM =",int(a)*int(b),"square units")
+
+# AREA OF RIGHT ANGLE TRIANGLE
+c = input("Base : ")
+d = input("Height : ")
+print("AREA OF Δ =",0.5*int(c)*int(d), "square units")
+
+# AREA OF CIRCLE
+e = input("Radius : ")
+print("AREA OF CIRCLE =",22/7*int(e)*int(e), "square units" )
+
+# AREA OF SQUARE
+d = input("edge : ")
+print("AREA OF SQUARE =",int(d)*int(d), "square units")
